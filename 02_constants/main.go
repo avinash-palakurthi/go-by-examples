13a31e3dcc4ad5941b2dd89bfc573b22218d1c56
Avinash <palakurthiavinash07@gmail.com>
2024-11-27 20:06:43 +0530

    constants

---
+++ 02_constants/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const fruit = "Apple"
+
+	// we cannot asign new value to the already declared variable
+
+	// fruit = "Mango"
+	fmt.Println("Fruit :", fruit)
+}
